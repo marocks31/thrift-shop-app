8d92c713a13360fd60de89ece04a762747b2e66a
marocks31 <mollyarisley@gmail.com>
2022-03-14 12:28:33 -0500

    practing api, create request is not working

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Photo.create(name: "1989 Garfield T-shirt", width: 200, height: 150)
+Photo.create(name: "1995 Candies Chunky Platform Wedge", width: 1024, height: 768)
+Photo.create(name: "1995 Butterfly Hair Clips", width: 150, height: 200)
